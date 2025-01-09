a6a701a964e1a4121868dea882da1928f9bac3d3
aths0711 <pathakatharv7@gmail.com>
2025-01-09 13:50:54 +0530

    Working XDictionary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [definition, setDefinition] = useState("");
+
+  const dictionary = [
+    {
+      word: "React",
+      meaning: "A JavaScript library for building user interfaces.",
+    },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." },
+  ];
+
+  const handleInputChange = (e) => {
+    setDefinition("");
+    let word = e.target.value;
+    setInputText(word);
+  };
+
+  const handleSearch = () => {
+    let wordInput = inputText;
+    let dictObj = dictionary.find(
+      ({ word }) => word.toLowerCase() === wordInput.toLowerCase()
+    );
+    let meaning = dictObj.meaning;
+    setDefinition(meaning);
+  };
   return (
     <div>
-      <h1> React Worked! </h1>
+      <h1> Dictionary App </h1>
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Search for a word..."
+          required
+          autoFocus
+        />
+        <button type="submit" onClick={handleSearch} cursor="pointer">
+          Search
+        </button>
+      </div>
+      <h4> Definition: </h4>
+      {definition && <p> {definition} </p>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
